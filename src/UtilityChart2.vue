0e0362df0706dcb6bc221ccbba8590e0b00608f9
Andrew Beales <andrewbeales@gmail.com>
2025-06-25 21:52:49 +0100

    can set utility line via intercept and slope v-model values (but have lost draggable ends and drag start cursor offset precision

--- src/UtilityChart2.vue
+++ src/UtilityChart2.vue
@@ -13,9 +13,9 @@
       <!-- 1:1 line from origin (thick blue line) -->
       <line
         x1="50"
-        :y1="lineStartY"
+        :y1="computedLineStartY"
         x2="250"
-        :y2="lineEndY"
+        :y2="computedLineEndY"
         stroke="blue"
         stroke-width="5"
         class="utility-line"
@@ -26,23 +26,27 @@
       />
       
       <!-- Circle at XY origin -->
-      <circle :cx="50" :cy="lineStartY" :r="CIRCLE_R" fill="red" stroke="darkred" stroke-width="1" />
+      <circle :cx="50" :cy="computedLineStartY" :r="CIRCLE_R" fill="red" stroke="darkred" stroke-width="1" />
 
       <!-- Circle at line end -->
-      <circle :cx="250" :cy="lineEndY" :r="CIRCLE_R" fill="green" stroke="darkgreen" stroke-width="1" />
+      <circle :cx="250" :cy="computedLineEndY" :r="CIRCLE_R" fill="green" stroke="darkgreen" stroke-width="1" />
     </svg>
   </div>
   <div>
-    <!-- Start: {{ lineStartY - 250 }}<br/>
+    <!-- Start: {{ computedLineStartY - 250 }}<br/>
     End: {{ (lineEndY - 250) * -1 }} -->
-    Line slope: {{ (lineEndY - lineStartY) * -1 / (250 - 50) }}<br/>
-    Intercept: {{ (lineStartY - 250) * -1 }}<br/>
-    set line slope <input type="number" v-model="lineStartY" />
+    Line slope: {{ lineSlope }}<br/>
+    Intercept: {{ intercept }}<br/>
+    set intercept <input type="number" v-model="intercept" /><br/>
+    set line slope <input type="number" v-model="lineSlope" />
+    <!-- lock line angle <input type="checkbox" v-model="lockLineAngle" /> -->
+
+
   </div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 export default {
   name: 'UtilityChart',
@@ -51,19 +55,33 @@ export default {
   },
   setup(props) {
 
-    const linearParams = ref({
-      slope: 1,
-      intercept: 0
-    })
-
     const CIRCLE_R = 5
+    const CHART_OFFSET_TOP = 50
+    const CHART_WIDTH = 200
+    const CHART_HEIGHT_ABOVE_ORIGIN = 200
+    // const CHART_HEIGHT_BELOW_ORIGIN = 200
 
-    const lineStartY = ref(250)
+    const intercept = ref(0)
+    const lineSlope = ref(1)
+    // const computedLineStartY = ref(250)
     const lineEndY = ref(50)
     const isDragging = ref(false)
-
+    const lockLineAngle = ref(false)
     const currentLineStartYDiff = ref(0)
     const currentLineEndYDiff = ref(0)
+
+    const computedLineStartY = computed(() => {
+      return (CHART_OFFSET_TOP + CHART_HEIGHT_ABOVE_ORIGIN) - intercept.value
+    })
+
+    const computedLineEndY = computed(() => {
+      const lineStartY = computedLineStartY.value
+      return (lineStartY - (lineSlope.value * CHART_WIDTH))
+    })
+
+    // const lineSlope = computed(() => {
+    //   return (lineEndY.value - computedLineStartY.value) * -1 / CHART_WIDTH
+    // })
 
     const startDrag = (event, type) => {
       console.log('startDrag', type)
@@ -72,22 +90,22 @@ export default {
 
       const cursorPoint = event.clientY - CIRCLE_R
 
-      console.log(cursorPoint, lineStartY.value)
+      console.log(cursorPoint, computedLineStartY.value)
 
       // Check if click is near the circle
-      if (Math.abs(cursorPoint - lineStartY.value) <= CIRCLE_R) {
+      if (Math.abs(cursorPoint - computedLineStartY.value) <= CIRCLE_R) {
         isDragging.value = 'lineStart'
         console.log('dragging line start', y)
       }
-      if (Math.abs(cursorPoint - lineEndY.value) <= CIRCLE_R) {
+      if (Math.abs(cursorPoint - computedLineEndY.value) <= CIRCLE_R) {
         isDragging.value = 'lineEnd'
         console.log('dragging line end', y)
       }
       if (type === 'utilityLine') {
         isDragging.value = 'utilityLine'
         console.log('dragging utility line', y)
-        const currentLineStartY = lineStartY.value
-        const currentLineEndY = lineEndY.value
+        const currentLineStartY = computedLineStartY.value
+        const currentLineEndY = computedLineEndY.value
         currentLineStartYDiff.value = (event.clientY - CIRCLE_R) - currentLineStartY
         currentLineEndYDiff.value = (event.clientY - CIRCLE_R) - currentLineEndY
       }
@@ -99,11 +117,13 @@ export default {
       // Constrain to Y axis only (x = 50) and bounds (50 to 250)
       // circleY.value = Math.max(50, Math.min(250, y))
       if (isDragging.value === 'lineStart') {
-        lineStartY.value = Math.max(50, Math.min(450, event.clientY))
+        computedLineStartY.value = Math.max(50, Math.min(450, event.clientY))
       } else if (isDragging.value === 'lineEnd') {
         lineEndY.value = Math.max(50, Math.min(450, event.clientY))
       } else if (isDragging.value === 'utilityLine') {
-        lineStartY.value = Math.max(50, Math.min(450, currentLineEndYDiff.value + event.clientY))
+        const newVal = Math.max(50, Math.min(450, currentLineEndYDiff.value + event.clientY))
+        intercept.value = 250 - newVal
+        // computedLineStartY.value = Math.max(50, Math.min(450, currentLineEndYDiff.value + event.clientY))
         lineEndY.value = Math.max(50, Math.min(450, currentLineStartYDiff.value + event.clientY))
         console.log('dragging utility line', currentLineStartYDiff, currentLineEndYDiff)
       }
@@ -111,7 +131,7 @@ export default {
 
     const stopDrag = () => {
       isDragging.value = false
-      console.log('stopDrag', lineStartY.value)
+      console.log('stopDrag', computedLineStartY.value)
     }
 
     const utilityFunction = (x) => {
@@ -137,10 +157,13 @@ export default {
 
     return {
       utilityFunction,
-      linearParams,
       CIRCLE_R,
-      lineStartY,
       lineEndY,
+      lineSlope,
+      intercept,
+      computedLineStartY,
+      computedLineEndY,
+      lockLineAngle,
       isDragging,
       startDrag,
       onDrag,
